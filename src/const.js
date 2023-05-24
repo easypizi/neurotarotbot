81ea1024e2a387cad5f73af66df4305cad5f0819
Evan Tolstof <itolstof@gmail.com>
2023-05-24 08:07:12 +0700

    add all stickers

--- src/const.js
+++ src/const.js
@@ -124,60 +124,116 @@ export const imageMappings = {
     "CAACAgQAAxkBAAOsZGeXCMseJca9ykpM8HhDhDtJjMEAAngIAAIKO2lSyzg9PgvWWSIvBA",
   "Мир / The World":
     "CAACAgQAAxkBAAOuZGeXFuASJcskJJLTxdYsdPAgkfYAAgsIAAI-VnFSKYNvlyQRbMQvBA",
-  "Туз Жезлов / Ace of Wands": null,
-  "Двойка Жезлов / Two of Wands": null,
-  "Тройка Жезлов / Three of Wands": null,
-  "Четверка Жезлов / Four of Wands": null,
-  "Пятерка Жезлов / Five of Wands": null,
-  "Шестерка Жезлов / Six of Wands": null,
-  "Семерка Жезлов / Seven of Wands": null,
-  "Восьмерка Жезлов / Eight of Wands": null,
-  "Девятка Жезлов / Nine of Wands": null,
-  "Десятка Жезлов / Ten of Wands": null,
-  "Паж Жезлов / Page of Wands": null,
-  "Рыцарь Жезлов / Knight of Wands": null,
-  "Королева Жезлов / Queen of Wands": null,
-  "Король Жезлов / King of Wands": null,
-  "Туз Кубков / Ace of Cups": null,
-  "Двойка Кубков / Two of Cups": null,
-  "Тройка Кубков / Three of Cups": null,
-  "Четверка Кубков / Four of Cups": null,
-  "Пятерка Кубков / Five of Cups": null,
-  "Шестерка Кубков / Six of Cups": null,
-  "Семерка Кубков / Seven of Cups": null,
-  "Восьмерка Кубков / Eight of Cups": null,
-  "Девятка Кубков / Nine of Cups": null,
-  "Десятка Кубков / Ten of Cups": null,
-  "Паж Кубков / Page of Cups": null,
-  "Рыцарь Кубков / Knight of Cups": null,
-  "Королева Кубков / Queen of Cups": null,
-  "Король Кубков / King of Cups": null,
-  "Туз Мечей / Ace of Swords": null,
-  "Двойка Мечей / Two of Swords": null,
-  "Тройка Мечей / Three of Swords": null,
-  "Четверка Мечей / Four of Swords": null,
-  "Пятерка Мечей / Five of Swords": null,
-  "Шестерка Мечей / Six of Swords": null,
-  "Семерка Мечей / Seven of Swords": null,
-  "Восьмерка Мечей / Eight of Swords": null,
-  "Девятка Мечей / Nine of Swords": null,
-  "Десятка Мечей / Ten of Swords": null,
-  "Паж Мечей / Page of Swords": null,
-  "Рыцарь Мечей / Knight of Swords": null,
-  "Королева Мечей / Queen of Swords": null,
-  "Король Мечей / King of Swords": null,
-  "Туз Пентаклей / Ace of Pentacles": null,
-  "Двойка Пентаклей / Two of Pentacles": null,
-  "Тройка Пентаклей / Three of Pentacles": null,
-  "Четверка Пентаклей / Four of Pentacles": null,
-  "Пятерка Пентаклей / Five of Pentacles": null,
-  "Шестерка Пентаклей / Six of Pentacles": null,
-  "Семерка Пентаклей / Seven of Pentacles": null,
-  "Восьмерка Пентаклей / Eight of Pentacles": null,
-  "Девятка Пентаклей / Nine of Pentacles": null,
-  "Десятка Пентаклей / Ten of Pentacles": null,
-  "Паж Пентаклей / Page of Pentacles": null,
-  "Рыцарь Пентаклей / Knight of Pentacles": null,
-  "Королева Пентаклей / Queen of Pentacles": null,
-  "Король Пентаклей / King of Pentacles": null,
+  "Туз Жезлов / Ace of Wands":
+    "CAACAgQAAxkBAAIBJGRrKSBMqaOWaEWacuIdVMiMiQmTAALyCQACWXtoUmiAphPBLxlDLwQ",
+  "Двойка Жезлов / Two of Wands":
+    "CAACAgQAAxkBAAIBJmRrKTHxj9TABPmwVzVBQkhcqRemAAKnCQACA91oUq8Tqw556wXPLwQ",
+  "Тройка Жезлов / Three of Wands":
+    "CAACAgQAAxkBAAIBKGRrKTNGauocOVCGUjUKAdypKO1NAAI9CAACjodxUvdQGw2_OgwTLwQ",
+  "Четверка Жезлов / Four of Wands":
+    "CAACAgQAAxkBAAIBKmRrKTUFU_0Nw47Au_6ZiFgg4o6EAAJdCgAC-HhoUlV9URo75nGDLwQ",
+  "Пятерка Жезлов / Five of Wands":
+    "CAACAgQAAxkBAAIBLGRrKTctx0Jlkl8U4S_mV5jhtdWWAALtBgACCv1xUsFIsgAB0oHcey8E",
+  "Шестерка Жезлов / Six of Wands":
+    "CAACAgQAAxkBAAIBLmRrKVvNtgG_hUPL8qNByPNoYYeKAALbBwACkn1pUlhDA_2EzT-BLwQ",
+  "Семерка Жезлов / Seven of Wands":
+    "CAACAgQAAxkBAAIBMGRrKVwIqz2lRNg-Mvkr7qtwld-lAAJ1CAAC-JVxUn-mssmszTdQLwQ",
+  "Восьмерка Жезлов / Eight of Wands":
+    "CAACAgQAAxkBAAIBMmRrKV5QYSP-pRkoMLflyLcvwM9OAAJqCQAC7C5pUgzLzhwE5J9RLwQ",
+  "Девятка Жезлов / Nine of Wands":
+    "CAACAgQAAxkBAAIBNGRrKWEIJKZzTTo-jLnDFKC0XNJyAAILCAAC-StoUjEiah18McNRLwQ",
+  "Десятка Жезлов / Ten of Wands":
+    "CAACAgQAAxkBAAIBNmRrKWPnF7E1qbBy1jGp8Y_8EcRWAALNBwACASJxUlXvSGJ1tj-sLwQ",
+  "Паж Жезлов / Page of Wands":
+    "CAACAgQAAxkBAAIBOGRrKZQ11PVycJ7ODGuh6Dqz6Mj2AAJlCAACfFhoUi-H9eRMw8aELwQ",
+  "Рыцарь Жезлов / Knight of Wands":
+    "CAACAgQAAxkBAAIBOmRrKZZOlkSSSautjfafUosXwvCiAAJFCAACOjpoUg89vurOfQPeLwQ",
+  "Королева Жезлов / Queen of Wands":
+    "CAACAgQAAxkBAAIBPGRrKZeojCtiW42olSvHRh0cgrp0AAJlCAACPd1oUglxW97vF1e1LwQ",
+  "Король Жезлов / King of Wands":
+    "CAACAgQAAxkBAAIBPmRrKZp1h77McVIqX2Gs5PgzV3UOAAKiCQAC8kZoUqPr4POPbYrELwQ",
+  "Туз Кубков / Ace of Cups":
+    "CAACAgQAAxkBAAIBXGRrKlxMSivuOGYiBR3mFQOG3dFoAAIvCAACFAhpUqWCDiVGbAUSLwQ",
+  "Двойка Кубков / Two of Cups":
+    "CAACAgQAAxkBAAIBXmRrKqx8e4hd5GGUM1w98zErZTUCAAKyBgAC451oUl-CRWmKBumMLwQ",
+  "Тройка Кубков / Three of Cups":
+    "CAACAgQAAxkBAAIBYGRrKq70UrPevuXNUNyLUGf5WOztAAJBBwAC4hhoUgpbAeqAygG2LwQ",
+  "Четверка Кубков / Four of Cups":
+    "CAACAgQAAxkBAAIBYmRrKrAW53H5Z7suy3LJI7UqV_7cAAIQCAAC0r5oUtFKAAHpbyiQni8E",
+  "Пятерка Кубков / Five of Cups":
+    "CAACAgQAAxkBAAIBZGRrKrJv_QGNh6CAT6gYTVTrQL4IAALFCAACTrZoUpAGrqLcFJkzLwQ",
+  "Шестерка Кубков / Six of Cups":
+    "CAACAgQAAxkBAAIBZmRrKtPJulY6bdNVj7_qOkUQH6g1AAIOCAAC8TRoUtvpVi4d_7-QLwQ",
+  "Семерка Кубков / Seven of Cups":
+    "CAACAgQAAxkBAAIBaGRrKtY5YApTh1yF_0tE8P3CUBDTAALGCAAC1LppUhSg9vwoYanSLwQ",
+  "Восьмерка Кубков / Eight of Cups":
+    "CAACAgQAAxkBAAIBamRrKtet9E0ShK-cMvylrcRFh4khAALoCAACV9RoUkqtF50hp4sFLwQ",
+  "Девятка Кубков / Nine of Cups":
+    "CAACAgQAAxkBAAIBbGRrKtnxb0yHTo6VygKi8UlnD0Q1AAK5CgACr2RpUpnja4AO_DmPLwQ",
+  "Десятка Кубков / Ten of Cups":
+    "CAACAgQAAxkBAAIBbmRrKtvawOcMOVsFC7NvUwfcouMNAAIECAACrsFoUv7mgIQwBNf1LwQ",
+  "Паж Кубков / Page of Cups":
+    "CAACAgQAAxkBAAIBcGRrKwhILzRg1THc12h3y2Kd3gTEAAK1CAACdyVoUuXKuvS_zL0qLwQ",
+  "Рыцарь Кубков / Knight of Cups":
+    "CAACAgQAAxkBAAIBcmRrKwopZWsPIlyxH3tXTcqqE5pxAAJPBwACgQJoUktNx-dc20B6LwQ",
+  "Королева Кубков / Queen of Cups":
+    "CAACAgQAAxkBAAIBdGRrKwwomQABDG8tcLi_vplo_RbmpAACOggAAvjacVJnQkDlMEXTcy8E",
+  "Король Кубков / King of Cups":
+    "CAACAgQAAxkBAAIBdmRrKw7kSLNu9OVDNlcLL_d7JlCWAALeCAACiRNpUodHLB02WUjcLwQ",
+  "Туз Мечей / Ace of Swords":
+    "CAACAgQAAxkBAAIBeGRrKy6WDw7Xa3ylbqVmdXmiomA7AALnBwACm6ZpUogerdkwxvcELwQ",
+  "Двойка Мечей / Two of Swords":
+    "CAACAgQAAxkBAAIBemRrKzfNA1mX-63L1qfdzozxU28NAAKYBwAC_vdpUpQWc6nSJ64ULwQ",
+  "Тройка Мечей / Three of Swords":
+    "CAACAgQAAxkBAAIBfGRrKzmaYiNdi83JqUR06SQQ3YNXAAJMCAACyLFwUm4-QZdCOpjPLwQ",
+  "Четверка Мечей / Four of Swords":
+    "CAACAgQAAxkBAAIBfmRrKzq2-NA-MqtI0REXXIZK42kjAAKqDAACiiRwUhpOT_CgnDq1LwQ",
+  "Пятерка Мечей / Five of Swords":
+    "CAACAgQAAxkBAAIBgGRrKzwCz1Vptt2gmIThkWAVCFjwAALhCwACKBVpUrzNbwUtM4BxLwQ",
+  "Шестерка Мечей / Six of Swords":
+    "CAACAgQAAxkBAAIBgmRrK2PKPjuwWAsHKr1WNKLiMNJgAAJGDAAC2OZpUl96tUbqVSJ7LwQ",
+  "Семерка Мечей / Seven of Swords":
+    "CAACAgQAAxkBAAIBhGRrK2XaK0MNKtDMGprrdYLoOGHcAAKeCQACW25oUgtUOdanLitVLwQ",
+  "Восьмерка Мечей / Eight of Swords":
+    "CAACAgQAAxkBAAIBhmRrK2djydk4pRGRlElJA_T1KFP_AAJFCQACj-9pUtZojL1fWh68LwQ",
+  "Девятка Мечей / Nine of Swords":
+    "CAACAgQAAxkBAAIBiGRrK2lJbsyu9yYxjjw1oMoJLJj0AAJaCAAC-KNpUgygTYZkcsTFLwQ",
+  "Десятка Мечей / Ten of Swords":
+    "CAACAgQAAxkBAAIBimRrK2vwzQABk1RWLolw7aAySpa08gACvgkAAliCaFKNKZKhzVc5ni8E",
+  "Паж Мечей / Page of Swords":
+    "CAACAgQAAxkBAAIBjGRrK5N1CiYDf67EJ1kPVgMydD01AAKeCgACL69pUpwCvP0_TS9ILwQ",
+  "Рыцарь Мечей / Knight of Swords":
+    "CAACAgQAAxkBAAIBjmRrK5Q8CYFyNO4W0IFJBlHtnC00AAIiCQACv6loUnQjvZATqpAuLwQ",
+  "Королева Мечей / Queen of Swords":
+    "CAACAgQAAxkBAAIBkGRrK5WLw1LxFpQltI5BPLOB-Q4_AAMJAAJHZGhSU3AhzZ6Wwl8vBA",
+  "Король Мечей / King of Swords":
+    "CAACAgQAAxkBAAIBkmRrK5djLCeQsPJY3nN_xWBdLphSAALHCQACe39oUrylkwLkrXTNLwQ",
+  "Туз Пентаклей / Ace of Pentacles":
+    "CAACAgQAAxkBAAIBQGRrKcTWLZJo6pDX435PrOITslyFAAItBwACS7VpUhNjYpnibY33LwQ",
+  "Двойка Пентаклей / Two of Pentacles":
+    "CAACAgQAAxkBAAIBQmRrKdFvJg7a7VRd5zKikY4rZa_jAAKCCQACquxoUvhpEX4om7f4LwQ",
+  "Тройка Пентаклей / Three of Pentacles":
+    "CAACAgQAAxkBAAIBRGRrKdSk4bRfIDcPPyyvZ3bUcQX7AAKRCgACv0VoUjqZvFn9fYNELwQ",
+  "Четверка Пентаклей / Four of Pentacles":
+    "CAACAgQAAxkBAAIBRmRrKdZIO73ASIyvSUkFko98RCBPAAJwCQACVStwUo_DTorswE9VLwQ",
+  "Пятерка Пентаклей / Five of Pentacles":
+    "CAACAgQAAxkBAAIBSGRrKdjPbdUj5zwigePA-qEW-cqrAAKZCQACyaBpUky5i13B06FmLwQ",
+  "Шестерка Пентаклей / Six of Pentacles":
+    "CAACAgQAAxkBAAIBSmRrKgJPGHPMys9E4w4jrnHX36OVAAKZCQACbWRoUuyUA5LmOekuLwQ",
+  "Семерка Пентаклей / Seven of Pentacles":
+    "CAACAgQAAxkBAAIBTGRrKgSJaHTpLrlv84ESvomu_yHKAALRCQACnHdoUpZK4RoqrjElLwQ",
+  "Восьмерка Пентаклей / Eight of Pentacles":
+    "CAACAgQAAxkBAAIBTmRrKgZrWjB_r07kVaqKFTAy_F4RAAL0CQAC80ppUs9yzl3549vGLwQ",
+  "Девятка Пентаклей / Nine of Pentacles":
+    "CAACAgQAAxkBAAIBUGRrKgfZsrfqYmKrXN3Mdjg9UjNvAAJsCAACGGdoUlmuRRbypk5vLwQ",
+  "Десятка Пентаклей / Ten of Pentacles":
+    "CAACAgQAAxkBAAIBUmRrKgmJEFC0aotvQ71jq1jr-fgKAAIYCQACDUZoUuyPBX4mtKbCLwQ",
+  "Паж Пентаклей / Page of Pentacles":
+    "CAACAgQAAxkBAAIBVGRrKjLP0q7wxbnR8_XwzFhadyo8AAJfCQACaOdpUrg00AABjB2cKy8E",
+  "Рыцарь Пентаклей / Knight of Pentacles":
+    "CAACAgQAAxkBAAIBVmRrKjPgS2OpkMryMvmSuIP_7763AAKZCQACax1oUsKAfqAlxQAByC8E",
+  "Королева Пентаклей / Queen of Pentacles":
+    "CAACAgQAAxkBAAIBWGRrKjVHsFiO30gV7LOlgR1NmTGPAALSCQACX-loUkIbKfBJb0PfLwQ",
+  "Король Пентаклей / King of Pentacles":
+    "CAACAgQAAxkBAAIBWmRrKjdtswABk-UCsnpWww0aBILzSQACCQgAAmsUaFLMDuxoUJZCLS8E",
 };
